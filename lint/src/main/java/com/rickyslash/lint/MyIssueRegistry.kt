442593e4284de5d82bfbbc5262ea8e8362109259
ricky-kiva <ricky.kiva@gmail.com>
2023-08-21 12:34:09 +0700

    build(gradle): add custom lint to registry

---
+++ lint/src/main/java/com/rickyslash/lint/MyIssueRegistry.kt
@@ -0,0 +1,14 @@
+package com.rickyslash.lint
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.CURRENT_API
+import com.android.tools.lint.detector.api.Issue
+
+class MyIssueRegistry: IssueRegistry() {
+    override val issues: List<Issue>
+        get() = listOf(
+            NamingPatternDetector.ISSUE_NAMING_PATTERN
+        )
+
+    override val api: Int = CURRENT_API
+}
