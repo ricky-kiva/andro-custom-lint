816af43186d4a63ec78c53668443b02b8980bd1d
ricky-kiva <ricky.kiva@gmail.com>
2023-08-21 12:35:00 +0700

    refactor(presentation-activity): check lint effect on non-camelcase class name

--- app/src/main/java/com/rickyslash/customlintapp/MAINActivity.kt
+++ app/src/main/java/com/rickyslash/customlintapp/MAINActivity.kt
@@ -3,7 +3,7 @@ package com.rickyslash.customlintapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MAINActivity : AppCompatActivity() { // warning on class naming after custom Lint
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
